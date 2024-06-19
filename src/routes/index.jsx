ac36af74b7e88779b509dcdc4af5b3e2a7107387
Nina Zhang <ninazhang@ninas-air.lan>
2024-06-19 17:17:10 -0400

    changed some

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,14 @@
+import React from 'react';
+
 export default function Index() {
-    return (
-      <p id="zero-state">
+  return (
+    <div id="zero-state">
+      <p>
         <i>Design a bouquet!</i>
       </p>
-    );
-  }
+      <button onClick={() => alert('Redirecting...')}>
+        Create Bouquet
+      </button>
+    </div>
+  );
+}
