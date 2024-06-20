524b934c46ecbbdee977b623f6a96128f102a809
Nina Zhang <ninazhang@ninas-air.lan>
2024-06-20 14:33:45 -0400

    added hover to link

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,58 +1,77 @@
+// ProductList.js
+
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import ProductCard from './ProductCard';
 
 const products = [
   {
+    id: 1,
     image: '/images/californiapoppy.png',
     name: 'California Poppy',
-    species: 'Eschscholzia californica', 
+    species: 'Eschscholzia californica',
   },
   {
+    id: 2,
     image: '/images/sunflower.jpg',
     name: 'Sunflower',
-    species: 'Helianthus annuus', 
+    species: 'Helianthus annuus',
   },
   {
+    id: 3,
     image: '/images/hollyhock.jpg',
     name: 'Hollyhock',
-    species: 'Alcea rosea', 
+    species: 'Alcea rosea',
   },
   {
+    id: 4,
     image: '/images/nasturtium.jpg',
     name: 'Garden Nasturtium',
-    species: 'Tropaeolum majus', 
+    species: 'Tropaeolum majus',
   },
   {
+    id: 5,
     image: '/images/valerian.jpg',
     name: 'Valerian',
-    species: 'Valeriana officinalis', 
+    species: 'Valeriana officinalis',
   },
   {
+    id: 6,
     image: '/images/evergreencandy.jpg',
     name: 'Evergreen Candytuft',
     species: 'Iberis sempervirens',
   },
   {
+    id: 7,
     image: '/images/cornflower.jpeg',
     name: 'Cornflower',
     species: 'Centaurea cyanus',
   },
   {
+    id: 8,
     image: '/images/aconite.jpg',
     name: 'Aconite',
-    species: 'Aconitum napellus', 
+    species: 'Aconitum napellus',
   },
 ];
 
-
 const ProductList = () => {
-  return (
-    <div className="product-list">
-      {products.map((product, index) => (
-        <ProductCard key={index} {...product} />
-      ))}
-    </div>
-  );
-};
+    return (
+      <div className="product-list">
+        {products.map((product) => (
+          <div key={product.id} className="product-container">
+            <ProductCard {...product} />
+            <NavLink
+              to={`/products/${product.id}`}
+              className="product-link"
+              activeClassName="active"
+            >
+              Learn more...
+            </NavLink>
+          </div>
+        ))}
+      </div>
+    );
+  };
 
 export default ProductList;
